6a05f25a531b38563e90c0fe4471a2f789779375
zhenkuan.zhao <zhenkuan.zhao@mljr.com>
2017-10-24 12:42:52 +0800

    spring-boot 项目搭建

--- spring-boot-test00/src/test/java/com/akuan/springboot/SpringbootTest00ApplicationTests01.java
+++ spring-boot-test00/src/test/java/com/akuan/springboot/SpringbootTest00ApplicationTests01.java
@@ -1,21 +1,17 @@
 package com.akuan.springboot;
 
+import com.akuan.springboot.controller.HelloWorldController;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.akuan.springboot.controller.HelloWorldController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 //
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@SpringBootApplication(scanBasePackageClasses = { MockServletContext.class })
